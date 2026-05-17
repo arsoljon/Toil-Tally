022103c1fae89b936cc8b5407b9bee860617e096
arsoljon <jdanarsola@gmail.com>
2026-05-17 03:08:19 -0500

    added button redirect to respective frames. removed unused code.

--- main.py
+++ main.py
@@ -4,24 +4,30 @@ import time
 
 
 class HomeFrame(tk.Frame):
-    def __init__(self, parent, totalTime, currentDate):
+    def __init__(self, parent, controller, clockInPage, addPage, totalTime, currentDate):
         super().__init__(parent)
         self.todaysTotalTime = "00:26:33"
         self.buttonLabels = ["Start", "Add"]
+        self.pages = [clockInPage, addPage]
+        style = "Arial"
+        size_offset = .8
+        size = int(16 * size_offset)
+
         #text
-        self.label_currentDate = tk.Label(self, font=("Arial", 16))
-        self.label_currentDate.pack(pady=20)
-        self.label_totalTime = tk.Label(self, font=("Arial", 16))
-        self.label_totalTime.pack(pady=20)
-        self.label_todaysTotalTime = tk.Label(self, text= f"Todays Total: {self.todaysTotalTime}", font=("Arial", 16))
-        self.label_todaysTotalTime.pack(pady=20)
+        self.label_currentDate = tk.Label(self, text=f"Date: {currentDate}", font=(style, size))
+        self.label_currentDate.grid(row=0, column=0, sticky="w")
+        self.label_totalTime = tk.Label(self, text=f"Total Time {totalTime}", font=(style, size))
+        self.label_totalTime.grid(row=0, column=1, sticky="e")
+        self.label_todaysTotalTime = tk.Label(self, text= f"Todays Total: {self.todaysTotalTime}", font=(style, size))
+        self.label_todaysTotalTime.grid(row=2, column=0, sticky="w")
         #buttons
-        self.button1 = tk.Button(self, text=buttonLabels[0], command=self.on_click)
-        self.button1.pack()
-        self.button2 = tk.Button(self, text=buttonLabels[1], command=self.on_click)
-        self.button2.pack()
+        self.button1 = tk.Button(self, text=self.buttonLabels[0], command=lambda: controller.show_frame(self.pages[0]))
+        self.button1.grid(row=1, column=0, sticky="w")
+        self.button2 = tk.Button(self, text=self.buttonLabels[1], command=lambda: controller.show_frame(self.pages[1]))
+        self.button2.grid(row=1, column=1, sticky="e")
 
     def on_click(self):
+        print()
         buttonMessage = "Button clicked!"
         print(buttonMessage)
 
@@ -31,17 +37,14 @@ class HomeFrame(tk.Frame):
         return self.todaysTotalTime
 
 class ClockedInFrame(tk.Frame):
-    def __init__(self, parent, totalTime, currentDate):
+    def __init__(self, parent, controller, totalTime, currentDate):
         super().__init__(parent)
         self.lengthOfSession = "01:02:03"
         self.buttonLabels = ["End", "Pause"]
- #text
+        #text
         style = "Arial"
         size_offset = .8
         size = int(16 * size_offset)
-        my_text = font.Font(family=style, size=size)
-        pixel_width = my_text.measure(currentDate)
-        index = 0
         
         self.label_currentDate = tk.Label(self, text=f"Date: {currentDate}", font=(style, size))
         self.label_currentDate.grid(row=0, column=0, sticky="w")
@@ -51,14 +54,18 @@ class ClockedInFrame(tk.Frame):
         self.label_lengthOfSession = tk.Label(self, text= f"Total: {self.lengthOfSession}", font=("Arial", 16))
         self.label_lengthOfSession.grid(row=2, column=0)
         #buttons
-        self.button1 = tk.Button(self, text=self.buttonLabels[0], command=self.on_click)
+        self.button1 = tk.Button(self, text=self.buttonLabels[0], command= lambda: self.on_click(controller, self.buttonLabels[0]))
         self.button1.grid(row=1, column=0)
-        self.button2 = tk.Button(self, text=self.buttonLabels[1], command=self.on_click)
+        self.button2 = tk.Button(self, text=self.buttonLabels[1], command= lambda: self.on_click(controller, self.buttonLabels[1]))
         self.button2.grid(row=1, column=1)
 
-    def on_click(self):
-        buttonMessage = "Button clicked!"
-        print(buttonMessage)
+    def on_click(self, controller, buttonLabel):
+        if(buttonLabel == self.buttonLabels[0]):
+            print(f"{self.buttonLabels[0]}!")
+            controller.show_frame(HomeFrame)
+        else:
+            print(f"{self.buttonLabels[1]}!")
+            controller.show_frame(PauseFrame)
 
     def getButtons(self):
         return self.buttonLabels
@@ -66,18 +73,15 @@ class ClockedInFrame(tk.Frame):
         return self.lengthOfSession
 
 class AddTimeFrame(tk.Frame):
-    def __init__(self, parent, totalTime, currentDate):
+    def __init__(self, parent, controller, totalTime, currentDate):
         super().__init__(parent)
         self.timeSelection= "00:00:00"
         self.buttonLabels = ["Add", "Cancel"]
-       #text
+        #text
         style = "Arial"
         size_offset = .8
         size = int(16 * size_offset)
-        my_text = font.Font(family=style, size=size)
-        pixel_width = my_text.measure(currentDate)
-        index = 0
-        
+
         self.label_currentDate = tk.Label(self, text=f"Date: {currentDate}", font=(style, size))
         self.label_currentDate.grid(row=0, column=0, sticky="w")
         self.label_totalTime = tk.Label(self, text=f"Total Time: {totalTime}", font=(style, size))
@@ -92,14 +96,18 @@ class AddTimeFrame(tk.Frame):
         minute = tk.Spinbox(self, from_=0, to=59, width=3, format="%02.0f")
         minute.grid(row=1, column=4)
         #buttons
-        self.button1 = tk.Button(self, text=self.buttonLabels[0], command=self.on_click)
+        self.button1 = tk.Button(self, text=self.buttonLabels[0], command= lambda: self.on_click(controller, self.buttonLabels[0]))
         self.button1.grid(row=1, column=0)
-        self.button2 = tk.Button(self, text=self.buttonLabels[1], command=self.on_click)
+        self.button2 = tk.Button(self, text=self.buttonLabels[1], command= lambda: self.on_click(controller, self.buttonLabels[1]))
         self.button2.grid(row=2, column=0)
 
-    def on_click(self):
-        buttonMessage = "Button clicked!"
-        print(buttonMessage)
+    def on_click(self, controller, buttonLabel):
+        if(buttonLabel == self.buttonLabels[0]):
+            print(f"{self.buttonLabels[0]}!")
+            controller.show_frame(HomeFrame)
+        else:
+            print(f"{self.buttonLabels[1]}!")
+            controller.show_frame(HomeFrame)
 
     def getButtons(self):
         return self.buttonLabels
@@ -107,7 +115,7 @@ class AddTimeFrame(tk.Frame):
         return self.timeSelection
 
 class PauseFrame(tk.Frame):
-    def __init__(self, parent, sessionLength, totalTime, currentDate):
+    def __init__(self, parent, controller, sessionLength, totalTime, currentDate):
         super().__init__(parent)
         self.lengthOfSession = sessionLength
         self.lengthOfPauseSession = "00:00:00"
@@ -115,23 +123,12 @@ class PauseFrame(tk.Frame):
         #text preprocessing
         root = self.winfo_toplevel()
         root.update_idletasks()
-        geometry_size, position= root.geometry().split("+", 1)
-        width, height = map(int, geometry_size.split("x"))
-        spacing = 5
-        row_offset =  height/5 
-        column_width_1 = int(width/2)
-        column_offset_2 = width/5
-        column_offset_3 = width/3
 
-
-        print(f"width : {width}")
         #text
         style = "Arial"
         size_offset = .8
         size = int(16 * size_offset)
         my_text = font.Font(family=style, size=size)
-        pixel_width = my_text.measure(currentDate)
-        index = 0
 
         self.label_currentDate = tk.Label(self, text=f"Date: {currentDate}", font=(style, size))
         self.label_currentDate.grid(row=0, column=0, sticky="w")
@@ -146,14 +143,18 @@ class PauseFrame(tk.Frame):
         self.label_lengthOfPausedSession = tk.Label(self, text= f"Pause for: {self.lengthOfPauseSession}", font=(style, size))
         self.label_lengthOfPausedSession.grid(row=3, column=0)
         #buttons
-        self.button1 = tk.Button(self, text=self.buttonLabels[0], command=self.on_click)
+        self.button1 = tk.Button(self, text=self.buttonLabels[0], command= lambda: self.on_click(controller, self.buttonLabels[0]))
         self.button1.grid(row=1, column=0)
-        self.button2 = tk.Button(self, text=self.buttonLabels[1], command=self.on_click)
+        self.button2 = tk.Button(self, text=self.buttonLabels[1], command= lambda: self.on_click(controller, self.buttonLabels[1]))
         self.button2.grid(row=1, column=1)
  
-    def on_click(self):
-        buttonMessage = "Button clicked!"
-        print(buttonMessage)
+    def on_click(self, controller, buttonLabel):
+        if(buttonLabel == self.buttonLabels[0]):
+            print(f"{self.buttonLabels[0]}!")
+            controller.show_frame(HomeFrame)
+        else:
+            print(f"{self.buttonLabels[1]}!")
+            controller.show_frame(ClockedInFrame)
 
     def getButtons(self):
         return self.buttonLabels
@@ -173,61 +174,21 @@ class AppUI(tk.Tk):
         container.pack(fill="both", expand=True)
 
         self.frames = {}
-        self.frames[HomeFrame] = HomeFrame(container, self.totalTime, self.currentDate)
-        self.frames[ClockedInFrame] = ClockedInFrame(container, self.totalTime, self.currentDate)
+        self.frames[HomeFrame] = HomeFrame(container, self, ClockedInFrame, AddTimeFrame, self.totalTime, self.currentDate)
+        self.frames[ClockedInFrame] = ClockedInFrame(container, self, self.totalTime, self.currentDate)
         session_length = self.frames[ClockedInFrame].getLengthOfSession()
-        self.frames[PauseFrame] = PauseFrame(container, session_length, self.totalTime, self.currentDate)
-        self.frames[AddTimeFrame] = AddTimeFrame(container, self.totalTime, self.currentDate)
+        self.frames[PauseFrame] = PauseFrame(container, self, session_length, self.totalTime, self.currentDate)
+        self.frames[AddTimeFrame] = AddTimeFrame(container, self, self.totalTime, self.currentDate)
         self.framesTitles = ["Default", "ClockedInScreen", "AddTimeScreen", "PauseScreen"]
         for frame in self.frames.values():
             frame.grid(row=0, column=0, sticky="nsew")
-
-        self.show_frame(AddTimeFrame)
-
+        self.show_frame(HomeFrame)
 
     def show_frame(self, frame_class):
         frame = self.frames[frame_class]
-        frame.label_currentDate.config(text=f"Date: {self.currentDate}")
-        frame.label_totalTime.config(text=f"Total Time: {self.totalTime}")
         frame.tkraise()
         
         
-
-windowTitles = ["Default", "ClockedInScreen", "AddTimeScreen", "PauseScreen"]
-currentDate = time.strftime("%Y-%m-%d")
-totalTime = "01:23:45"
-todaysTotalTime = "00:26:33"
-buttonLabels = ["Start", "Add"]
-
-windowSize = "400x300+100+600"
-
-prompt_currentDate = f"Date: {currentDate}"
-prompt_totalTime = f"Total Time: {totalTime}"
-prompt_todaysTotalTime = f"Accumulated Today: {todaysTotalTime}"
-
-buttonLabel = "Click me"
-'''
-default = tk.Tk()
-default.title(windowTitles[0])
-default.geometry(windowSize)
-
-label_currentDate = tk.Label(default, text=prompt_currentDate , font=("Arial", 16))
-label_currentDate.pack(pady=20)
-label_totalTime = tk.Label(default, text=prompt_totalTime , font=("Arial", 16))
-label_totalTime.pack(pady=20)
-label_todaysTotalTime = tk.Label(default, text=prompt_todaysTotalTime , font=("Arial", 16))
-label_todaysTotalTime.pack(pady=20)
-
-def on_click():
-    ButtonMessage = "Button clicked!"
-    print(ButtonMessage)
-
-button1 = tk.Button(default, text=buttonLabels[0], command=on_click)
-button1.pack()
-button2 = tk.Button(default, text=buttonLabels[1], command=on_click)
-button2.pack()
-'''
-
 if __name__ == "__main__":
     app = AppUI()
     app.mainloop()
